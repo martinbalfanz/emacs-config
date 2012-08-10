9dd0de57f1e4978048418020ee69fd1fe7e2dba8
Martin Balfanz <martin.balfanz@gmail.com>
2012-08-10 14:46:36 +0200

    occur-mode configured.

--- init.el
+++ init.el
@@ -247,6 +247,33 @@ Deletes whitespace at join."
 
 
 ;;;;
+;;;; occur
+;;;;
+
+(add-hook 'occur-mode-hook
+          (lambda ()
+            (next-error-follow-minor-mode 1)
+            (hl-line-mode 1)
+            (local-set-key (kbd "n") 'next-line)
+            (local-set-key (kbd "p") 'previous-line)))
+
+(add-hook 'occur-edit-mode-hook
+          (lambda ()
+            (local-unset-key (kbd "n"))
+            (local-unset-key (kbd "p"))))
+
+(defadvice occur
+  (after mb-switch-to-occur last () activate)
+  "Switch to occur window automatically."
+  (other-window 1))
+
+(defadvice ibuffer-do-occur
+  (after mb-ibuffer-switch-to-occur last () activate)
+  "Switch to occur window automatically."
+  (other-window 1))
+
+
+;;;;
 ;;;; paredit
 ;;;; thanks to https://github.com/danlei
 ;;;;
