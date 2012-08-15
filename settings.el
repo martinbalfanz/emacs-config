313727c20dd40005540f11d86ccb3cc8f805c59e
Martin Balfanz <martin.balfanz@gmail.com>
2012-08-15 17:40:48 +0200

    Added some customized faces.

--- settings.el
+++ settings.el
@@ -9,4 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(js2-highlight-vars-face ((t (:inherit highlight))))
+ '(match ((t (:inherit highlight))))
+ '(w3m-image-anchor ((t (:inherit highlight)))))
