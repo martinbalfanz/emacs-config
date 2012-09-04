eb5605da287c378e470f2dea3e1acb2aa0b954f5
Martin Balfanz <martin.balfanz@gmail.com>
2012-09-04 20:11:00 +0200

    Changed mu4e colors.

--- settings.el
+++ settings.el
@@ -11,5 +11,7 @@
  ;; If there is more than one, they won't work right.
  '(js2-highlight-vars-face ((t (:inherit highlight))))
  '(match ((t (:inherit highlight))))
+ '(mu4e-flagged-face ((t (:foreground "#990044" :weight bold))))
  '(mu4e-header-highlight-face ((t (:inherit highlight :underline nil :weight bold))))
+ '(mu4e-unread-face ((t (:inherit font-lock-constant-face :weight bold))))
  '(w3m-image-anchor ((t (:inherit highlight)))))
