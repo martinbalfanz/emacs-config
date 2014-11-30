113ea791fef8949d0a6a1193e1693e83410f8ce7
Martin Balfanz <me@martinbalfanz.com>
2014-11-30 16:24:43 +0100

    Auto-update settings.el

--- settings.el
+++ settings.el
@@ -3,21 +3,29 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(ansi-color-names-vector
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(background-color "#fdf6e3")
+ '(background-mode light)
+ '(cursor-color "#657b83")
+ '(custom-safe-themes
+   (quote
+    ("cf08ae4c26cacce2eebff39d129ea0a21c9d7bf70ea9b945588c1c66392578d1" "52588047a0fe3727e3cd8a90e76d7f078c9bd62c0b246324e557dfa5112e0d0c" "5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+ '(foreground-color "#657b83"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(google-contacts-familyname ((t (:weight bold :height 1))))
- '(google-contacts-givenname ((t (:weight bold :height 1))))
- '(google-contacts-header ((t (:weight bold :height 1))))
- '(google-contacts-organization-name ((t (:slant italic :height 1))))
- '(google-contacts-organization-title ((t (:height 1))))
- '(google-contacts-rel ((t (:weight bold :height 1))))
- '(js2-highlight-vars-face ((t (:inherit highlight))))
+ '(google-contacts-familyname ((t (:weight bold :height 1))) t)
+ '(google-contacts-givenname ((t (:weight bold :height 1))) t)
+ '(google-contacts-header ((t (:weight bold :height 1))) t)
+ '(google-contacts-organization-name ((t (:slant italic :height 1))) t)
+ '(google-contacts-organization-title ((t (:height 1))) t)
+ '(google-contacts-rel ((t (:weight bold :height 1))) t)
+ '(js2-highlight-vars-face ((t (:inherit highlight))) t)
  '(match ((t (:inherit highlight))))
  '(mu4e-flagged-face ((t (:inherit font-lock-constant-face :foreground "#990044" :weight bold))))
  '(mu4e-header-highlight-face ((t (:inherit highlight :underline nil :weight bold))))
  '(mu4e-unread-face ((t (:inherit font-lock-constant-face :weight bold))))
- '(w3m-image-anchor ((t (:inherit highlight)))))
+ '(w3m-image-anchor ((t (:inherit highlight))) t))
