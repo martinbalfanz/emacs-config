08b164a90f91d1a2f97c5a37b6562ae562746699
Martin Balfanz <me@martinbalfanz.com>
2015-07-13 21:33:56 +0200

    Added my old set of util functions.

---
+++ modules/my-util.el
@@ -0,0 +1,106 @@
+;; random utils
+;; TODO cleanup
+
+(defun indent-buffer ()
+  "Reindents the whole buffer."
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-defun ()
+  "Indent the current defun."
+  (interactive)
+  (save-excursion
+    (mark-defun)
+    (indent-region (region-beginning) (region-end))))
+
+(defun google ()
+  "Google the selected region if any, display a query prompt otherwise."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string
+     (if (region-active-p)
+         (buffer-substring (region-beginning) (region-end))
+       (read-string "Google: "))))))
+
+(defun insert-date (prefix)
+  "Insert the current date. With prefix-argument, use ISO format. With
+   two prefix arguments, write out the day and month name."
+  (interactive "P")
+  (let ((format (cond ((not prefix) "%d.%m.%Y")
+                      ((equal prefix '(4)) "%Y-%m-%d")
+                      ((equal prefix '(16)) "%A, %d. %B %Y")))
+        (system-time-locale "en_US"))
+    (insert (format-time-string format))))
+
+(defun define-keys (mode-map keybindings)
+  "Takes a mode map, and a list of (key function-designator)
+lists. The functions are bound to the keys in the given mode-map.
+Keys are in kbd format."
+  (mapc (lambda (keybinding)
+          (destructuring-bind (key function) keybinding
+            (define-key mode-map (read-kbd-macro key) function)))
+        keybindings))
+
+(defun global-set-keys (keybindings)
+  "Takes a list of (key function-designator) lists.
+The functions are globally bound to the keys. Keys
+are in kbd format."
+  (mapc (lambda (keybinding)
+          (destructuring-bind (key function) keybinding
+            (global-set-key (read-kbd-macro key) function)))
+        keybindings))
+
+(defun mb-kill-and-join-forward (&optional arg)
+  "If at end of line, join with following; otherwise kill line.
+Deletes whitespace at join."
+  (interactive "P")
+  (if (and (eolp) (not (bolp)))
+      (delete-indentation t)
+    (kill-line arg)))
+
+(defun mb-newline-beneath ()
+  "Add a new newline beneath the current line."
+  (interactive)
+  (end-of-line)
+  (newline-and-indent))
+
+(defun mb-newline-above ()
+  "Add a newline above the current line."
+  (interactive)
+  (previous-line)
+  (end-of-line)
+  (newline-and-indent))
+
+(defun mb-beginning-of-line (&optional arg)
+  "Toggle point between beginning of line and first non-whitespace."
+  (interactive "P")
+  (if (and (bolp) (not arg))
+      (back-to-indentation)
+    (beginning-of-line arg)))
+
+(defun buffer-contains-regex-p (regex)
+  "Check if buffer contains a given REGEX."
+  (save-excursion
+    (save-match-data
+      (goto-char (point-min))
+      (if (search-forward-regexp regex nil t)
+          t
+        nil))))
+
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+(provide 'my-util)
